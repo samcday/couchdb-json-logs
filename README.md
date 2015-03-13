b226f67341f1ae0eb7c1ec38d05a31153186e1e6
Sam Day <sday@atlassian.com>
2015-03-14 01:40:30 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# couchdb-json-logs
+
+Raw CouchDB log lines go in. JSON log records go out.
+
+```bash
+  npm -g install couchdb-json-logs
+
+  cat /var/log/couchdb/couch.log | couchdb-json-logs
+```
+
+Uses [bunyan](https://github.com/trentm/node-bunyan) and [isaacs/couchdb-log-parse](https://github.com/isaacs/couchdb-log-parse) to do the magic.
+
+## License 
+
+[MIT License](LICENSE).
---
+++ index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var bunyan = require("bunyan");
+
+var logName = process.argv[2] || "couchdb";
+var log = bunyan.createLogger({name: logName});
+var parser = new (require("couchdb-log-parse"));
+
+process.stdin.pipe(parser);
+
+var currentRecord;
+
+parser.on("message", function(record) {
+  if (record.raw) {
+    if (currentRecord) {
+      if (!currentRecord.supplemented) {
+        currentRecord.msg += "\n";
+        currentRecord.supplemented = true;
+      }
+      currentRecord.msg += record.raw;
+    }
+    return;
+  }
+
+  if (currentRecord) {
+    log[currentRecord.level](currentRecord.record, currentRecord.msg);
+    clearTimeout(currentRecord.timeout);
+    currentRecord = null;
+  }
+
+  var msg = record.message;
+  var level = record.level;
+
+  record.time = record.date;
+  delete record.date;
+  delete record.message;
+  delete record.level;
+  delete record.pid;
+
+  // If dump doesn't look like an erlang object then push it into msg.
+  if (record.dump && record.dump[0] !== "{") {
+    msg = msg + record.dump;
+    delete record.dump;
+  }
+
+  if ("http" === record.type) {
+    msg = record.method + " " + record.url;
+  }
+
+  currentRecord = {record: record, msg: msg, level: level};
+  currentRecord.timeout = setTimeout(function() {
+
+  }, 100);
+});
